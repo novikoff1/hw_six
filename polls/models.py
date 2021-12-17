7e1194e081810995c8d35ced9dfaef58b71f0d22
“Novikov <novikofff1@gmail.com>
2021-12-17 08:09:56 +0200

    add flake and travis

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,5 @@
 import datetime
+
 from django.contrib import admin
 from django.db import models
 from django.utils import timezone
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.views import generic
 from django.utils import timezone
+from django.views import generic
+
 from .models import Choice, Question
 
 
