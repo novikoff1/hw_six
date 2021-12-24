5c695b8fc39f2f8d7aaeaf03f87b5abdbe4a3f55
“Novikov <novikofff1@gmail.com>
2021-12-25 00:35:33 +0200

    add command

---
+++ polls/management/commands/comm.py
@@ -0,0 +1,27 @@
+import random
+import string
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+from faker import Faker
+
+
+def password():
+    chars = string.digits + string.ascii_letters
+    return ''.join(random.choice(chars) for _ in range(20))
+
+
+class Commands(BaseCommand):
+    users = 'Fake users'  # noqa: A003
+
+    def add_arguments(self, parser):
+        parser.add_argument('total', type=int, choices=range(1, 11))
+
+    def handle(self, *args, **kwargs):
+        total = kwargs['total']
+        faker = Faker()
+        for i in range(total):
+            User.objects.create_user(username=faker.name(),
+                                     email=faker.email(),
+                                     password=password())
